0f3bc340a4377da766e1797bd4beefa8b31209c3
Ryan Trauntvein <djdefi@users.noreply.github.com>
2023-03-17 02:08:13 +0000

    playoff attention

--- script.rb
+++ script.rb
@@ -336,7 +336,7 @@
                     f.write("<tr class='color-bg-danger'>")
                 # If a team is one of the next two highest-placed finishers in each conference, color the row yellow with 'color-bg-warning'
                 elsif team['wildCardRank'].to_i <= 2
-                    f.write("<tr class='color-bg-warning'>")
+                    f.write("<tr class='color-bg-attention'>")
                 else
                 end
                     f.write("<td class='p-2 border'>#{team['name']}</td>
