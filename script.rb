e493542aba9963850435b7e84cdca05659e06f4c
Ryan Trauntvein <djdefi@users.noreply.github.com>
2023-05-02 22:25:13 +0000

    playoffs

--- script.rb
+++ script.rb
@@ -294,7 +294,34 @@
         end
         return trophy_info
     end
-    
+
+    # Function to get the playoff standings from https://statsapi.web.nhl.com/api/v1/tournaments/playoffs/?expand=round.series,schedule.game.seriesSummary&season=20222023
+    # Example API response:
+    # {"copyright"=>"NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2023. All Rights Reserved.", "id"=>1, "name"=>"Playoffs", "season"=>"20222023", "defaultRound"=>2, "rounds"=>[{"number"=>1, "code"=>1, "names"=>{"name"=>"First Round", "shortName"=>"R1"}, "format"=>{"name"=>"BO7", "description"=>"Best of 7", "numberOfGames"=>7, "numberOfWins"=>4}, "series"=>[{"seriesNumber"=>1, "seriesCode"=>"A", "names"=>{"matchupName"=>"Bruins (1) vs. Panthers (WC2)", "matchupShortName"=>"BOS v FLA", "teamAbbreviationA"=>"BOS", "teamAbbreviationB"=>"FLA", "seriesSlug"=>"bruins-vs-panthers-series-a"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030117, "gameNumber"=>7, "gameLabel"=>"Game 7", "necessary"=>true, "gameCode"=>117, "gameTime"=>"2023-04-30T22:30:00Z", "seriesStatus"=>"Panthers win 4-3", "seriesStatusShort"=>"FLA wins 4-3"}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>6, "name"=>"Boston Bruins", "link"=>"/api/v1/teams/6"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>3, "losses"=>4}}, {"team"=>{"id"=>13, "name"=>"Florida Panthers", "link"=>"/api/v1/teams/13"}, "seed"=>{"type"=>"WC2", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>4, "losses"=>3}}]}, {"seriesNumber"=>2, "seriesCode"=>"B", "names"=>{"matchupName"=>"Maple Leafs (2) vs. Lightning (3)", "matchupShortName"=>"TOR v TBL", "teamAbbreviationA"=>"TOR", "teamAbbreviationB"=>"TBL", "seriesSlug"=>"maple-leafs-vs-lightning-series-b"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030126, "gameNumber"=>6, "gameLabel"=>"Game 6", "necessary"=>true, "gameCode"=>126, "gameTime"=>"2023-04-29T23:00:00Z", "seriesStatus"=>"Maple Leafs win 4-2", "seriesStatusShort"=>"TOR wins 4-2"}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>10, "name"=>"Toronto Maple Leafs", "link"=>"/api/v1/teams/10"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>2}}, {"team"=>{"id"=>14, "name"=>"Tampa Bay Lightning", "link"=>"/api/v1/teams/14"}, "seed"=>{"type"=>"3", "rank"=>3, "isTop"=>false}, "seriesRecord"=>{"wins"=>2, "losses"=>4}}]}, {"seriesNumber"=>3, "seriesCode"=>"C", "names"=>{"matchupName"=>"Hurricanes (1) vs. Islanders (WC1)", "matchupShortName"=>"CAR v NYI", "teamAbbreviationA"=>"CAR", "teamAbbreviationB"=>"NYI", "seriesSlug"=>"hurricanes-vs-islanders-series-c"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030136, "gameNumber"=>6, "gameLabel"=>"Game 6", "necessary"=>true, "gameCode"=>136, "gameTime"=>"2023-04-28T23:00:00Z", "seriesStatus"=>"Hurricanes win 4-2", "seriesStatusShort"=>"CAR wins 4-2"}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>12, "name"=>"Carolina Hurricanes", "link"=>"/api/v1/teams/12"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>2}}, {"team"=>{"id"=>2, "name"=>"New York Islanders", "link"=>"/api/v1/teams/2"}, "seed"=>{"type"=>"WC1", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>2, "losses"=>4}}]}, {"seriesNumber"=>4, "seriesCode"=>"D", "names"=>{"matchupName"=>"Devils (2) vs. Rangers (3)", "matchupShortName"=>"NJD v NYR", "teamAbbreviationA"=>"NJD", "teamAbbreviationB"=>"NYR", "seriesSlug"=>"devils-vs-rangers-series-d"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030147, "gameNumber"=>7, "gameLabel"=>"Game 7", "necessary"=>true, "gameCode"=>147, "gameTime"=>"2023-05-02T00:00:00Z", "seriesStatus"=>"Devils win 4-3", "seriesStatusShort"=>"NJD wins 4-3"}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>1, "name"=>"New Jersey Devils", "link"=>"/api/v1/teams/1"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>3}}, {"team"=>{"id"=>3, "name"=>"New York Rangers", "link"=>"/api/v1/teams/3"}, "seed"=>{"type"=>"3", "rank"=>3, "isTop"=>false}, "seriesRecord"=>{"wins"=>3, "losses"=>4}}]}, {"seriesNumber"=>5, "seriesCode"=>"E", "names"=>{"matchupName"=>"Avalanche (1) vs. Kraken (WC1)", "matchupShortName"=>"COL v SEA", "teamAbbreviationA"=>"COL", "teamAbbreviationB"=>"SEA", "seriesSlug"=>"avalanche-vs-kraken-series-e"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030157, "gameNumber"=>7, "gameLabel"=>"Game 7", "necessary"=>true, "gameCode"=>157, "gameTime"=>"2023-05-01T01:30:00Z", "seriesStatus"=>"Kraken win 4-3", "seriesStatusShort"=>"SEA wins 4-3"}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>21, "name"=>"Colorado Avalanche", "link"=>"/api/v1/teams/21"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>3, "losses"=>4}}, {"team"=>{"id"=>55, "name"=>"Seattle Kraken", "link"=>"/api/v1/teams/55"}, "seed"=>{"type"=>"WC1", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>4, "losses"=>3}}]}, {"seriesNumber"=>6, "seriesCode"=>"F", "names"=>{"matchupName"=>"Stars (2) vs. Wild (3)", "matchupShortName"=>"DAL v MIN", "teamAbbreviationA"=>"DAL", "teamAbbreviationB"=>"MIN", "seriesSlug"=>"stars-vs-wild-series-f"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030166, "gameNumber"=>6, "gameLabel"=>"Game 6", "necessary"=>true, "gameCode"=>166, "gameTime"=>"2023-04-29T01:30:00Z", "seriesStatus"=>"Stars win 4-2", "seriesStatusShort"=>"DAL wins 4-2"}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>25, "name"=>"Dallas Stars", "link"=>"/api/v1/teams/25"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>2}}, {"team"=>{"id"=>30, "name"=>"Minnesota Wild", "link"=>"/api/v1/teams/30"}, "seed"=>{"type"=>"3", "rank"=>3, "isTop"=>false}, "seriesRecord"=>{"wins"=>2, "losses"=>4}}]}, {"seriesNumber"=>7, "seriesCode"=>"G", "names"=>{"matchupName"=>"Golden Knights (1) vs. Jets (WC2)", "matchupShortName"=>"VGK v WPG", "teamAbbreviationA"=>"VGK", "teamAbbreviationB"=>"WPG", "seriesSlug"=>"golden-knights-vs-jets-series-g"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030175, "gameNumber"=>5, "gameLabel"=>"Game 5", "necessary"=>true, "gameCode"=>175, "gameTime"=>"2023-04-28T02:00:00Z", "seriesStatus"=>"Golden Knights win 4-1", "seriesStatusShort"=>"VGK wins 4-1"}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>54, "name"=>"Vegas Golden Knights", "link"=>"/api/v1/teams/54"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>1}}, {"team"=>{"id"=>52, "name"=>"Winnipeg Jets", "link"=>"/api/v1/teams/52"}, "seed"=>{"type"=>"WC2", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>1, "losses"=>4}}]}, {"seriesNumber"=>8, "seriesCode"=>"H", "names"=>{"matchupName"=>"Oilers (2) vs. Kings (3)", "matchupShortName"=>"EDM v LAK", "teamAbbreviationA"=>"EDM", "teamAbbreviationB"=>"LAK", "seriesSlug"=>"oilers-vs-kings-series-h"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030186, "gameNumber"=>6, "gameLabel"=>"Game 6", "necessary"=>true, "gameCode"=>186, "gameTime"=>"2023-04-30T02:00:00Z", "seriesStatus"=>"Oilers win 4-2", "seriesStatusShort"=>"EDM wins 4-2"}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>1}, "matchupTeams"=>[{"team"=>{"id"=>22, "name"=>"Edmonton Oilers", "link"=>"/api/v1/teams/22"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>4, "losses"=>2}}, {"team"=>{"id"=>26, "name"=>"Los Angeles Kings", "link"=>"/api/v1/teams/26"}, "seed"=>{"type"=>"3", "rank"=>3, "isTop"=>false}, "seriesRecord"=>{"wins"=>2, "losses"=>4}}]}]}, {"number"=>2, "code"=>2, "names"=>{"name"=>"Second Round", "shortName"=>"R2"}, "format"=>{"name"=>"BO7", "description"=>"Best of 7", "numberOfGames"=>7, "numberOfWins"=>4}, "series"=>[{"seriesNumber"=>1, "seriesCode"=>"I", "names"=>{"matchupName"=>"Maple Leafs (2) vs. Panthers (WC2)", "matchupShortName"=>"TOR v FLA", "teamAbbreviationA"=>"TOR", "teamAbbreviationB"=>"FLA", "seriesSlug"=>"maple-leafs-vs-panthers-series-i"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030211, "gameNumber"=>1, "gameLabel"=>"Game 1", "necessary"=>true, "gameCode"=>211, "gameTime"=>"2023-05-02T23:00:00Z", "seriesStatus"=>"", "seriesStatusShort"=>""}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>2}, "matchupTeams"=>[{"team"=>{"id"=>10, "name"=>"Toronto Maple Leafs", "link"=>"/api/v1/teams/10"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}, {"team"=>{"id"=>13, "name"=>"Florida Panthers", "link"=>"/api/v1/teams/13"}, "seed"=>{"type"=>"WC2", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}]}, {"seriesNumber"=>2, "seriesCode"=>"J", "names"=>{"matchupName"=>"Hurricanes (1) vs. Devils (2)", "matchupShortName"=>"CAR v NJD", "teamAbbreviationA"=>"CAR", "teamAbbreviationB"=>"NJD", "seriesSlug"=>"hurricanes-vs-devils-series-j"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030221, "gameNumber"=>1, "gameLabel"=>"Game 1", "necessary"=>true, "gameCode"=>221, "gameTime"=>"2023-05-03T23:00:00Z", "seriesStatus"=>"", "seriesStatusShort"=>""}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>2}, "matchupTeams"=>[{"team"=>{"id"=>12, "name"=>"Carolina Hurricanes", "link"=>"/api/v1/teams/12"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}, {"team"=>{"id"=>1, "name"=>"New Jersey Devils", "link"=>"/api/v1/teams/1"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>false}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}]}, {"seriesNumber"=>3, "seriesCode"=>"K", "names"=>{"matchupName"=>"Stars (2) vs. Kraken (WC1)", "matchupShortName"=>"DAL v SEA", "teamAbbreviationA"=>"DAL", "teamAbbreviationB"=>"SEA", "seriesSlug"=>"stars-vs-kraken-series-k"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030231, "gameNumber"=>1, "gameLabel"=>"Game 1", "necessary"=>true, "gameCode"=>231, "gameTime"=>"2023-05-03T01:30:00Z", "seriesStatus"=>"", "seriesStatusShort"=>""}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>2}, "matchupTeams"=>[{"team"=>{"id"=>25, "name"=>"Dallas Stars", "link"=>"/api/v1/teams/25"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>true}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}, {"team"=>{"id"=>55, "name"=>"Seattle Kraken", "link"=>"/api/v1/teams/55"}, "seed"=>{"type"=>"WC1", "rank"=>4, "isTop"=>false}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}]}, {"seriesNumber"=>4, "seriesCode"=>"L", "names"=>{"matchupName"=>"Golden Knights (1) vs. Oilers (2)", "matchupShortName"=>"VGK v EDM", "teamAbbreviationA"=>"VGK", "teamAbbreviationB"=>"EDM", "seriesSlug"=>"golden-knights-vs-oilers-series-l"}, "currentGame"=>{"seriesSummary"=>{"gamePk"=>2022030241, "gameNumber"=>1, "gameLabel"=>"Game 1", "necessary"=>true, "gameCode"=>241, "gameTime"=>"2023-05-04T01:30:00Z", "seriesStatus"=>"", "seriesStatusShort"=>""}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>2}, "matchupTeams"=>[{"team"=>{"id"=>54, "name"=>"Vegas Golden Knights", "link"=>"/api/v1/teams/54"}, "seed"=>{"type"=>"1", "rank"=>1, "isTop"=>true}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}, {"team"=>{"id"=>22, "name"=>"Edmonton Oilers", "link"=>"/api/v1/teams/22"}, "seed"=>{"type"=>"2", "rank"=>2, "isTop"=>false}, "seriesRecord"=>{"wins"=>0, "losses"=>0}}]}]}, {"number"=>3, "code"=>3, "names"=>{"name"=>"Conference Finals", "shortName"=>"CF"}, "format"=>{"name"=>"BO7", "description"=>"Best of 7", "numberOfGames"=>7, "numberOfWins"=>4}, "series"=>[{"seriesCode"=>"M", "names"=>{"matchupName"=>"", "matchupShortName"=>"", "teamAbbreviationA"=>"", "teamAbbreviationB"=>""}, "currentGame"=>{"seriesSummary"=>{"gameLabel"=>""}}, "conference"=>{"id"=>6, "name"=>"Eastern", "link"=>"/api/v1/conferences/6"}, "round"=>{"number"=>3}}, {"seriesCode"=>"N", "names"=>{"matchupName"=>"", "matchupShortName"=>"", "teamAbbreviationA"=>"", "teamAbbreviationB"=>""}, "currentGame"=>{"seriesSummary"=>{"gameLabel"=>""}}, "conference"=>{"id"=>5, "name"=>"Western", "link"=>"/api/v1/conferences/5"}, "round"=>{"number"=>3}}]}, {"number"=>4, "code"=>4, "names"=>{"name"=>"Stanley Cup Final", "shortName"=>"SCF"}, "format"=>{"name"=>"BO7", "description"=>"Best of 7", "numberOfGames"=>7, "numberOfWins"=>4}, "series"=>[{"seriesCode"=>"O", "names"=>{"matchupName"=>"", "matchupShortName"=>"", "teamAbbreviationA"=>"", "teamAbbreviationB"=>""}, "currentGame"=>{"seriesSummary"=>{"gameLabel"=>""}}, "conference"=>{"link"=>"/api/v1/conferences/null"}, "round"=>{"number"=>4}}]}]}
+    def get_playoff_standings(season)
+        response = HTTParty.get('https://statsapi.web.nhl.com/api/v1/tournaments/playoffs/?expand=round.series,schedule.game.seriesSummary&season=' + season.to_s)
+        puts response.parsed_response
+        return response.parsed_response
+    end
+
+    # Load the playoff standings response into a hash
+    def load_playoff_standings(season)
+        playoff_standings_hash = get_playoff_standings(season)
+        return playoff_standings_hash
+    end
+
+    # Function to determine which two combined years to query for the current seasion such as 20222023
+    def current_season_years
+        current_year = Time.now.year
+        current_month = Time.now.month
+        if current_month < 9
+            current_season_years = (current_year - 1).to_s + current_year.to_s
+        else
+            current_season_years = current_year.to_s + (current_year + 1).to_s
+        end
+        return current_season_years
+    end
+
     # Output to file a primer css styled HTML table of the sorted array of fan team stats by league rank
     def output_sorted_fan_team_stats_by_league_rank(fan_team_hash)
         sorted_team_stats_array = sort_fan_team_stats_by_league_rank(fan_team_hash)
@@ -374,10 +401,13 @@
         <span class='IssueLabel color-bg-severe-emphasis color-fg-on-emphasis mr-1'>Wildcard hopeful</span>
         <span class='IssueLabel color-bg-danger-emphasis color-fg-on-emphasis mr-1'>Not in playoff contention</span>
         <div class='border-top border-bottom border-gray-light mt-3 pt-3'>
+
         <h2 class='color-fg-success'>#{trophy_info['name']}</h2>
         <img src='#{trophy_info['imageUrl']}' alt='#{trophy_info['name']}' style='width: 200px;'>
         <p class='color-fg-muted'>#{trophy_info['description']}</p>
         </div>
+        <a href='./playoffs.html' class='btn btn-primary'>Playoff Standings</a>
+
     </body>
     </html>")
         end
@@ -388,4 +418,61 @@
     
     if File.file?("_site/index.html")
         puts "File created at: #{File.absolute_path("_site/index.html")}"
+    end
+
+    # Output to file a primer css styled HTML table of the playoff standings for the current season
+    def output_playoff_standings(current_season)
+        playoff_standings = get_playoff_standings(current_season)
+      
+        def generate_series_html(series)
+          <<-HTML
+            <div class="series mb-3">
+              <h3 class="color-fg-success">#{series["names"]["matchupName"]}</h3>
+              <p>Series Status: #{series["currentGame"]["seriesSummary"]["seriesStatus"]}</p>
+              <p>Game Number: #{series["currentGame"]["seriesSummary"]["gameNumber"]}</p>
+              <p>Game Time: #{series["currentGame"]["seriesSummary"]["gameTime"]}</p>
+            </div>
+          HTML
+        end
+      
+        def generate_round_html(round)
+          series_html = round["series"].map { |series| generate_series_html(series) }.join("\n")
+          <<-HTML
+            <div class="round mb-5">
+              <h2 class="color-fg-success">#{round["names"]["name"]}</h2>
+              #{series_html}
+            </div>
+          HTML
+        end
+      
+        File.open("_site/playoffs.html", "w") do |f|
+          f.write(<<-HTML)
+      <!DOCTYPE html>
+      <html lang='en'>
+      <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>NHL Playoff Standings</title>
+        <link rel='stylesheet' href='https://unpkg.com/@primer/css@^20.2.4/dist/primer.css'>
+      </head>
+      <body class='m-2' data-color-mode='auto' data-light-theme='light' data-dark-theme='dark_dimmed'>
+        <div class='container-xl px-3 px-md-4 px-lg-5 mt-3'>
+          <h1 class='color-fg-success'>NHL Playoff Standings</h1>
+          <h2 class='color-fg-success'>#{current_season}</h2>
+          #{playoff_standings["rounds"].map { |round| generate_round_html(round) }.join("\n")}
+        </div>
+        <a href='./index.html' class='btn btn-primary'>Regular Season Standings</a>
+      </body>
+      </html>
+          HTML
+        end
+      end
+      
+      
+
+    current_season = current_season_years
+    output_playoff_standings(current_season)
+
+    if File.file?("_site/playoffs.html")
+        puts "File created at: #{File.absolute_path("_site/playoffs.html")}"
     end
