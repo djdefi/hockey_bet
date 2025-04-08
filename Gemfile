28903294c4e67189bd60d912e2a4c66fac909755
Ryan Trauntvein <djdefi@github.com>
2025-04-08 06:51:22 +0000

    fix things

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'httparty'
 gem 'csv'
 gem 'tzinfo'
+gem 'base64' # Required for Ruby 3.4+
+gem 'bigdecimal' # Required for Ruby 3.4+
 
 group :development, :test do
   gem 'rspec'
@@ -10,5 +12,3 @@ group :development, :test do
   gem 'axe-core-rspec'
   gem 'simplecov', require: false
 end
-
-gem "base64", "~> 0.2.0"
--- lib/team_mapping.rb
+++ lib/team_mapping.rb
@@ -4,35 +4,35 @@
 # Method to match a team name from fan_team.csv to a team abbreviation
 def map_team_name_to_abbrev(team_name, teams)
   return nil if team_name.nil? || team_name.empty?
-  
+
   team_name_downcase = team_name.strip.downcase
-  
+
   # Try exact abbreviation match first
   exact_match = teams.find { |team| team['teamAbbrev']['default'].downcase == team_name_downcase }
   return exact_match['teamAbbrev']['default'] if exact_match
-  
+
   # Next, try to match against full team name
-  name_match = teams.find do |team| 
+  name_match = teams.find do |team|
     full_name = team['teamName']['default'].downcase
-    full_name == team_name_downcase || 
-    full_name.include?(team_name_downcase) || 
+    full_name == team_name_downcase ||
+    full_name.include?(team_name_downcase) ||
     team_name_downcase.include?(full_name)
   end
   return name_match['teamAbbrev']['default'] if name_match
-  
+
   # Try city/region name matching
   place_match = teams.find do |team|
     place_name = team.dig('placeName', 'default')&.downcase || ''
-    place_name == team_name_downcase || 
-    place_name.include?(team_name_downcase) || 
+    place_name == team_name_downcase ||
+    place_name.include?(team_name_downcase) ||
     team_name_downcase.include?(place_name)
   end
   return place_match['teamAbbrev']['default'] if place_match
-  
+
   # Levenshtein distance for nickname matching (allows for minor typos)
   best_score = Float::INFINITY
   best_match = nil
-  
+
   teams.each do |team|
     # Try fuzzy match against team name, place name, and any other identifiers
     team_identifiers = [
@@ -40,7 +40,7 @@ def map_team_name_to_abbrev(team_name, teams)
       team.dig('placeName', 'default')&.downcase,
       team['teamAbbrev']['default'].downcase
     ].compact
-    
+
     team_identifiers.each do |identifier|
       score = levenshtein_distance(team_name_downcase, identifier)
       if score < best_score && score < [team_name_downcase.length, identifier.length].min / 3
@@ -49,7 +49,7 @@ def map_team_name_to_abbrev(team_name, teams)
       end
     end
   end
-  
+
   return best_match ? best_match['teamAbbrev']['default'] : nil
 end
 
@@ -57,15 +57,15 @@ end
 def levenshtein_distance(s, t)
   m = s.length
   n = t.length
-  
+
   return m if n == 0
   return n if m == 0
-  
+
   d = Array.new(m+1) { Array.new(n+1) }
-  
+
   (0..m).each { |i| d[i][0] = i }
   (0..n).each { |j| d[0][j] = j }
-  
+
   (1..n).each do |j|
     (1..m).each do |i|
       cost = s[i-1] == t[j-1] ? 0 : 1
@@ -76,30 +76,30 @@ def levenshtein_distance(s, t)
       ].min
     end
   end
-  
+
   d[m][n]
 end
 
-# Method to help map team names to abbreviations consistently
-def map_team_name_to_abbrev(team_name, teams)
-  return nil if team_name.nil? || team_name.empty?
-  
-  team_name_downcase = team_name.strip.downcase
-  
-  # Try direct mapping first
-  if TEAM_NAME_MAPPING.key?(team_name_downcase)
-    return TEAM_NAME_MAPPING[team_name_downcase]
-  end
-  
-  # Then try fuzzy matching from our teams data
-  matched_team = teams.find do |team|
-    team_full_name = team['teamName']['default'].downcase
-    team_abbrev = team['teamAbbrev']['default'].downcase
-    
-    team_full_name.include?(team_name_downcase) || 
-    team_name_downcase.include?(team_full_name) || 
-    team_abbrev == team_name_downcase
-  end
-  
-  return matched_team ? matched_team['teamAbbrev']['default'] : nil
-end
+# Constant mapping of common team name variations to official abbreviations
+TEAM_NAME_MAPPING = {
+  'boston' => 'BOS',
+  'bruins' => 'BOS',
+  'leafs' => 'TOR',
+  'maple leafs' => 'TOR',
+  'toronto' => 'TOR',
+  'florida' => 'FLA',
+  'panthers' => 'FLA',
+  'tampa' => 'TBL',
+  'lightning' => 'TBL',
+  'tampa bay' => 'TBL',
+  'detroit' => 'DET',
+  'red wings' => 'DET',
+  'buffalo' => 'BUF',
+  'sabres' => 'BUF',
+  'ottawa' => 'OTT',
+  'senators' => 'OTT',
+  'sens' => 'OTT',
+  'montreal' => 'MTL',
+  'canadiens' => 'MTL',
+  'habs' => 'MTL'
+}
